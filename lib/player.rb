ca3710128261834ae66b9e9f728601c198ba1361
juroga_new <juanjrgast@gmail.com>
2023-02-17 09:03:06 +0100

    Add @color to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Player
-  def initialize(name)
-    attr_reader :name
+  def initialize(name, color)
+    attr_reader :name, :color
     attr_accessor :origin, :destination
 
     @name = name
+    @color = color
     @origin = [] #This will choose the coordinate of the piece that current_player wants to move
     @destination = [] #This will choose the coordinate of the square that current_player wants to move the selected piece
   end
