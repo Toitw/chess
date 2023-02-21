83b877640af980dda2d56c4cdbe27814b11e2f94
juroga_new <juanjrgast@gmail.com>
2023-02-21 18:16:48 +0100

    Added module with unicode_pieces for the visual part

---
+++ lib/visual.rb
@@ -0,0 +1,10 @@
+module ChessUnicodePieces
+    UNICODE_PIECES = {
+        king:   { white: "\u2654", black: "\u265A" },
+        queen:  { white: "\u2655", black: "\u265B" },
+        rook:   { white: "\u2656", black: "\u265C" },
+        bishop: { white: "\u2657", black: "\u265D" },
+        knight: { white: "\u2658", black: "\u265E" },
+        pawn:   { white: "\u2659", black: "\u265F" }
+      }
+end
