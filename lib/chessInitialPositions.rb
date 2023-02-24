802592ed7e3773816770d848350455a5a94ce93e
juroga_new <juanjrgast@gmail.com>
2023-02-24 19:21:45 +0100

    Rearrange INITIAL_POSITIONS so it is easier to visualize it

--- lib/chessInitialPositions.rb
+++ lib/chessInitialPositions.rb
@@ -1,11 +1,30 @@
 module ChessInitialPositions
-    INITIAL_POSITIONS = {
-      rook:   { white: [[0, 0], [7, 0]], black: [[0, 7], [7, 7]] },
-      knight: { white: [[0, 1], [7, 1]], black: [[0, 6], [7, 6]] },
-      bishop: { white: [[0, 2], [7, 2]], black: [[0, 5], [7, 5]] },
-      queen:  { white: [[0, 3]], black: [[7, 3]] },
-      king:   { white: [[0, 4]], black: [[7, 4]] },
-      pawn:   { white: (0..7).map { |i| [i, 1] }, black: (0..7).map { |i| [i, 6] } }
+  INITIAL_POSITIONS = {
+    rook: {
+      white: [[0, 0], [7, 0]],
+      black: [[0, 7], [7, 7]]
+    },
+    knight: {
+      white: [[0, 1], [7, 1]],
+      black: [[0, 6], [7, 6]]
+    },
+    bishop: {
+      white: [[0, 2], [7, 2]],
+      black: [[0, 5], [7, 5]]
+    },
+    queen: {
+      white: [[0, 3]],
+      black: [[7, 3]]
+    },
+    king: {
+      white: [[0, 4]],
+      black: [[7, 4]]
+    },
+    pawn: {
+      white: (0..7).map { |i| [i, 1] },
+      black: (0..7).map { |i| [i, 6] }
     }
+  }
 end
+
   
