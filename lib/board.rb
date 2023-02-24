772d46505fa15349c422bed30b0a67533806f036
juroga_new <juanjrgast@gmail.com>
2023-02-24 19:08:31 +0100

    Added #display_board

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,10 @@
 
 require_relative "chessInitialPositions"
 require_relative "piece"
+require_relative "visual"
 
 class Board
+  include ChessInitialPositions
   include ChessInitialPositions
 
   attr_accessor :board
@@ -20,6 +22,26 @@ class Board
           @board[column][row] = Piece.new(type, color)
         end
       end
+    end
+  end
+
+  def display_board
+    puts "    0   1   2   3   4   5   6   7 "
+    puts "  ---------------------------------"
+    @board.each_with_index do |row, y|
+      print "#{y} |"
+
+      row.each_with_index do |piece, x|
+        if piece.nil?
+          print "   "
+        else
+          color = piece.color == :white ? :white : :black
+          symbol = UNICODE_PIECES[piece.type][color]
+          print " #{symbol} "
+        end
+        print "|"
+      end
+      puts "\n  ---------------------------------"
     end
   end
 end
