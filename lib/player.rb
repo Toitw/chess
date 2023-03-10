7d775f78ba5bf83daec2cc5cae86d80b33ecb8b4
juroga_new <juanjrgast@gmail.com>
2023-03-10 15:43:45 +0100

    Add an instance variable @king_checked to work the method #check_king_threat

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Player
-  attr_accessor :origin, :destination, :name, :color
+  attr_accessor :origin, :destination, :name, :color, :king_checked
   def initialize(name, color = nil)
     @name = name
     @color = color
     @origin = [] #This will choose the coordinate of the piece that current_player wants to move
     @destination = [] #This will choose the coordinate of the square that current_player wants to move the selected piece
+    @king_checked = false #When the king is threathed by a opponent piece
   end
 end
